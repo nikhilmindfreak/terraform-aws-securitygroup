f04c2959699bc90368ee15fd266f921584ea009c
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-06-04 18:26:42 -0400

    Initial commit

--- variables.tf
+++ variables.tf
@@ -34,7 +34,7 @@ variable "outbound_rules" {    # ww gave default user have to over ride it
         from_port = 0   # we gave access to all
         to_port = 0
         protocol = "-1"  # -1 all protocols/all ports
-        cidr_blocks = ["0.0.0.0/1"]  # blocks its a list
+        cidr_blocks = ["0.0.0.0/0"]  # blocks its a list
     }
   ]
 }
